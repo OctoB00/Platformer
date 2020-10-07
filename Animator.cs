75413f36186afe7b3f3ed3a4e4700a74048d40b8
OliverMal <70704225+OliverMal@users.noreply.github.com>
2020-10-07 16:50:39 +0200

    Add files via upload

---
+++ Animator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+
+namespace Platformer
+{
+    public class Animator
+    {
+        private static Texture texture = new Texture("SimpleRobot.png");
+        private static Sprite AnimatedSprite = new Sprite(texture);
+        int width;
+        int height;
+        int currentFrame = 0;
+        int maxFrame = 1;
+        public Animator(int width, int height, int frames)
+        {
+            this.width = width;
+            this.height = height;
+            this.maxFrame = frames - 1;
+        }
+        public void Animate(Sprite sprite)
+        {
+            sprite.TextureRect = new IntRect(width * currentFrame, 0, width, height);
+            currentFrame++;
+            if (currentFrame > maxFrame)
+            {
+                currentFrame = 0;
+            }
+        }
+        public Sprite GetSprite()
+        {
+            return AnimatedSprite;
+        }
+    }
+}
